23f3192a7934512216869dbb2f6c8a31d4d4c0b6
Lee Si Woong <silverttthin@naver.com>
2025-08-22 04:09:07 +0900

    init: 스프링부트 프로젝트 초기화

---
+++ src/main/java/com/igemoney/igemoney_BE/IgemoneyBeApplication.java
@@ -0,0 +1,13 @@
+package com.igemoney.igemoney_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgemoneyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgemoneyBeApplication.class, args);
+	}
+
+}
