ad452f70164d0d88c010a2f456d1450d72667679
Lee Si Woong <silverttthin@naver.com>
2025-08-22 04:15:18 +0900

    chore: 아직 db 뭐쓸지 미확이라 일단 디비 세팅 배제하고 프로젝트 실행 가능하게 옵션걸었음

--- src/main/java/com/igemoney/igemoney_BE/IgemoneyBeApplication.java
+++ src/main/java/com/igemoney/igemoney_BE/IgemoneyBeApplication.java
@@ -2,8 +2,11 @@ package com.igemoney.igemoney_BE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+// todo: db 의존성 설치하면 exclude 속성 지워버리기
+@SpringBootApplication(exclude={ DataSourceAutoConfiguration.class})
 public class IgemoneyBeApplication {
 
 	public static void main(String[] args) {
