ac9f75ba461f4300481c3da653dc7032cc474f9b
Lee Si Woong <silverttthin@naver.com>
2025-08-28 23:26:04 +0900

    feat: mysql 연결

--- src/main/java/com/igemoney/igemoney_BE/IgemoneyBeApplication.java
+++ src/main/java/com/igemoney/igemoney_BE/IgemoneyBeApplication.java
@@ -2,11 +2,9 @@ package com.igemoney.igemoney_BE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-// todo: db 의존성 설치하면 exclude 속성 지워버리기
-@SpringBootApplication(exclude={ DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class IgemoneyBeApplication {
 
 	public static void main(String[] args) {
