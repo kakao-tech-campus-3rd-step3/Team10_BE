2e9d56b335ca359bcf612a731eaf52d7c1c2e5ce
Lee Si Woong <silverttthin@naver.com>
2025-09-01 17:40:32 +0900

    feat: cd 테스트용 간이 컨트롤러 생성

---
+++ src/main/java/com/igemoney/igemoney_BE/fortest/testController.java
@@ -0,0 +1,15 @@
+package com.igemoney.igemoney_BE.fortest;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// cd가 잘 작동되는지 확인하기 위한 임시 컨트롤러입니다. 추후 삭제예정입니다
+@RestController
+public class testController {
+	@GetMapping("/test")
+	public String test() {
+		return "test";
+	}
+}
