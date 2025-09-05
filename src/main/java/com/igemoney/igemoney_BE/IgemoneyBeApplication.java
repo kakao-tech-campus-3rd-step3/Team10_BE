f71e72c8e801c5c28459ba065dc290f528b857a9
JAEIL1999 <pkyj040410@gmail.com>
2025-09-05 14:13:37 +0900

    Feat/user setup

--- src/main/java/com/igemoney/igemoney_BE/IgemoneyBeApplication.java
+++ src/main/java/com/igemoney/igemoney_BE/IgemoneyBeApplication.java
@@ -2,7 +2,9 @@ package com.igemoney.igemoney_BE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication()
 public class IgemoneyBeApplication {
 
---
+++ src/main/java/com/igemoney/igemoney_BE/user/UserModel.java
@@ -0,0 +1,33 @@
+package com.igemoney.igemoney_BE.user;
+
+import com.igemoney.igemoney_BE.BaseEntity.BaseEntity;
+import jakarta.persistence.*;
+
+import java.security.Timestamp;
+
+@Entity
+@Table(name = "user")
+public class UserModel extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String userId;
+
+    @Column(name = "nickname", unique = true, nullable = false)
+    private String nickname;
+
+
+    @Column(name = "oauth_id", unique = true)
+    private String oauthId;
+
+    @Column(name = "rating_point")
+    private int rating_point;
+
+    @Column(name = "consecutive_attendence_days")
+    private int consecutive_attendence;
+
+    @Column(name = "today_solved_count")
+    private int today_count;
+
+    @Column(name = "is_active")
+    private boolean is_active;
+}
