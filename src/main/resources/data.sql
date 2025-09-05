43765acb51ee7aadfd3a817c96b6da67a2f546e2
Lee Si Woong <silverttthin@naver.com>
2025-09-05 17:04:02 +0900

    feat: 기본 유저 레코드 생성 구현

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (nickname, oauth_id, rating_point, consecutive_attendance, today_count, is_active, created_at, updated_at)
+VALUES ('테스트유저', 'test_oauth_123', 0, 1, 0, 1, NOW(), NOW());
