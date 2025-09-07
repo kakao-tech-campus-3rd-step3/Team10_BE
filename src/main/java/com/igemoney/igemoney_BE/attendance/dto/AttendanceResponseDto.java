fa7b9c10a1bc187af9874e2e229b6172507b090b
hopencode <newalfo1006@gmail.com>
2025-09-07 19:44:50 +0900

    refactor: 컨벤션에 맞춰 DTO를 record로 작성

--- src/main/java/com/igemoney/igemoney_BE/attendance/dto/AttendanceResponseDto.java
+++ src/main/java/com/igemoney/igemoney_BE/attendance/dto/AttendanceResponseDto.java
@@ -1,17 +1,6 @@
 package com.igemoney.igemoney_BE.attendance.dto;
 
-public class AttendanceResponseDto {
-    private boolean attendanceToday;
-    private int todaySolvedCount;
-
-    public AttendanceResponseDto(boolean attendanceToday, int todaySolvedCount) {
-        this.attendanceToday = attendanceToday;
-        this.todaySolvedCount = todaySolvedCount;
-    }
-
-    public boolean isAttendanceToday() { return attendanceToday; }
-    public void setAttendanceToday(boolean attendanceToday) { this.attendanceToday = attendanceToday; }
-
-    public int getTodaySolvedCount() { return todaySolvedCount; }
-    public void setTodaySolvedCount(int todaySolvedCount) { this.todaySolvedCount = todaySolvedCount; }
-}
+public record AttendanceResponseDto(
+        boolean attendanceToday,
+        int todaySolvedCount
+) {}
