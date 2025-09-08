165c8e73266bdacbb1c6eeaf65065b379289463c
Lee Si Woong <silverttthin@naver.com>
2025-09-08 22:11:46 +0900

    feat: 빈 생성자 protected로 전환 + 퀴즈 생성자 직접 추가

--- src/main/java/com/igemoney/igemoney_BE/quiz/entity/Quiz.java
+++ src/main/java/com/igemoney/igemoney_BE/quiz/entity/Quiz.java
@@ -13,10 +13,8 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "quiz")
 @Getter
-@Setter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Builder
 public class Quiz extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -48,4 +46,18 @@ public class Quiz extends BaseEntity {
 
     @Column(name = "correct_rate", precision = 5, scale = 2)
     private BigDecimal correctRate;
+
+
+    @Builder
+    public Quiz(QuizTopic topic, String questionTitle, QuestionType questionType, String questionData, DifficultyLevel difficultyLevel, String explanation, Integer questionOrder) {
+        this.topic = topic;
+        this.questionTitle = questionTitle;
+        this.questionType = questionType;
+        this.questionData = questionData;
+        this.difficultyLevel = difficultyLevel;
+        this.explanation = explanation;
+        this.questionOrder = questionOrder;
+        this.correctRate = BigDecimal.ZERO;
+    }
+
 }
