98faa306d000c662d1d070d71f6c53b7c67c9843
Lee Si Woong <silverttthin@naver.com>
2025-09-08 22:13:49 +0900

    feat: 유저 정답 기록용 테이블 작성

---
+++ src/main/java/com/igemoney/igemoney_BE/quiz/entity/UserQuizSolved.java
@@ -0,0 +1,28 @@
+package com.igemoney.igemoney_BE.quiz.entity;
+
+
+import com.igemoney.igemoney_BE.common.entity.BaseEntity;
+import com.igemoney.igemoney_BE.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserQuizSolved extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userQuizSolvedId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "quiz_id", nullable = false)
+	private Quiz quiz;
+
+}
