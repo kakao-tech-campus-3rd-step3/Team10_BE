7f2b6bf4eb44f502aac976c0995be9cc08dbd89e
Lee Si Woong <silverttthin@naver.com>
2025-09-10 02:24:25 +0900

    feat: 누락된 생성자 추가

--- src/main/java/com/igemoney/igemoney_BE/quiz/entity/UserQuizCorrect.java
+++ src/main/java/com/igemoney/igemoney_BE/quiz/entity/UserQuizCorrect.java
@@ -5,6 +5,7 @@ import com.igemoney.igemoney_BE.common.entity.BaseEntity;
 import com.igemoney.igemoney_BE.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -24,5 +25,11 @@ public class UserQuizCorrect extends BaseEntity {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "quiz_id", nullable = false)
 	private Quiz quiz;
+
+	@Builder
+	public UserQuizCorrect(User user, Quiz quiz) {
+		this.user = user;
+		this.quiz = quiz;
+	}
 
 }
