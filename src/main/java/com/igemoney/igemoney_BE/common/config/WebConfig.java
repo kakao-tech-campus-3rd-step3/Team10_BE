3aadc06bb1bfa40f0d9f006a78765a9d24820f20
Lee Si Woong <silverttthin@naver.com>
2025-09-21 03:59:20 +0900

    feat: 인가 기능 구현 - 인터셉터에서 @Authenticated이 붙은 메서드 검증 - 토큰이 없거나 sub이 없으면 401 리턴 - 해당 어노테이션이 붙은 컨트롤러 메서드의 인자에서 RequestAttribute("userId")로 토큰의 sub 추출 가능

--- src/main/java/com/igemoney/igemoney_BE/common/config/WebConfig.java
+++ src/main/java/com/igemoney/igemoney_BE/common/config/WebConfig.java
@@ -1,13 +1,19 @@
 package com.igemoney.igemoney_BE.common.config;
 
 
+import com.igemoney.igemoney_BE.common.interceptor.AuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+	private final AuthenticationInterceptor authenticationInterceptor;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
@@ -21,4 +27,13 @@ public class WebConfig implements WebMvcConfigurer {
 			.allowCredentials(true)
 			.maxAge(3600);
 	}
+
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authenticationInterceptor)
+			.addPathPatterns("/**")
+			.excludePathPatterns("/api/user/**");
+	}
+
 }
