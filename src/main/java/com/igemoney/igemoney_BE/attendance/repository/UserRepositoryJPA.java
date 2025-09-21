dff064fa5ca318e19e26c4b7ed4ff31cd3907d0e
hopencode <newalfo1006@gmail.com>
2025-09-21 11:56:46 +0900

    refactor: User 레포지토리 사용하도록 연결

--- src/main/java/com/igemoney/igemoney_BE/attendance/repository/UserRepositoryJPA.java
+++
@@ -1,9 +0,0 @@
-package com.igemoney.igemoney_BE.attendance.repository;
-
-import com.igemoney.igemoney_BE.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepositoryJPA extends JpaRepository<User, String>{
-    Optional<User> findByOauthId(String oauthId);
-}
--- src/main/java/com/igemoney/igemoney_BE/attendance/service/AttendanceService.java
+++ src/main/java/com/igemoney/igemoney_BE/attendance/service/AttendanceService.java
@@ -2,7 +2,7 @@ package com.igemoney.igemoney_BE.attendance.service;
 
 import com.igemoney.igemoney_BE.attendance.dto.AttendanceResponseDto;
 import com.igemoney.igemoney_BE.user.entity.User;
-import com.igemoney.igemoney_BE.attendance.repository.UserRepositoryJPA;
+import com.igemoney.igemoney_BE.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,16 +12,16 @@ import java.util.NoSuchElementException;
 @Transactional
 public class AttendanceService {
 
-    private final UserRepositoryJPA userRepository;
+    private final UserRepository userRepository;
 
-    public AttendanceService(UserRepositoryJPA userRepository) {
+    public AttendanceService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     private static final int ATTENDANCE_THRESHOLD = 5;
 
-    public AttendanceResponseDto getTodayAttendance(String oauthId) {
-        User user = userRepository.findByOauthId(oauthId)
+    public AttendanceResponseDto getTodayAttendance(Long kakaoOauthId) {
+        User user = userRepository.findByKakaoOauthId(kakaoOauthId)
                 .orElseThrow(() -> new NoSuchElementException("User not found"));
 
         int solvedCount = user.getTodayCount();
@@ -30,8 +30,8 @@ public class AttendanceService {
         return new AttendanceResponseDto(attendanceToday, solvedCount);
     }
 
-    public void incrementTodaySolvedCount(String oauthId) {
-        User user = userRepository.findByOauthId(oauthId)
+    public void incrementTodaySolvedCount(Long kakaoOauthId) {
+        User user = userRepository.findByKakaoOauthId(kakaoOauthId)
                 .orElseThrow(() -> new NoSuchElementException("User not found"));
 
         user.increaseTodaySolvedCount();
