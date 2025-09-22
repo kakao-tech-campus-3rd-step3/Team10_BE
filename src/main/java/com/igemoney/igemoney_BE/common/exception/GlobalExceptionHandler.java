0b2e82d23f10ace0a695df4dacbe6d978964a753
Lee Si Woong <silverttthin@naver.com>
2025-09-22 17:14:47 +0900

    fix: pr 피드백 2번 반영 - servletResponse에 직접 코드, json을 기록하는 것보다 그냥 이미 만들어진 커스텀 예외를 던지는 것으로 변경 - 이 과정에서 401 에러들을 던지는 에러들만을 핸들링하는 메서드 구조로 변경 (메서드들을 나열하기 보단 어노테이션 내에 에러들을 나열하는게 더 낫겠단 생각)

--- src/main/java/com/igemoney/igemoney_BE/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/igemoney/igemoney_BE/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.igemoney.igemoney_BE.common.exception;
 
+import com.igemoney.igemoney_BE.common.exception.user.NoUserIdTokenException;
 import com.igemoney.igemoney_BE.common.exception.user.NotRegisteredUserException;
+import com.igemoney.igemoney_BE.common.exception.user.UnvalidJwtTokenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,11 +11,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(NotRegisteredUserException.class)
-    public ResponseEntity<ErrorResponse> handleNotRegisteredUserException(NotRegisteredUserException e) {
+    @ExceptionHandler({
+        NotRegisteredUserException.class,
+        NoUserIdTokenException.class,
+        UnvalidJwtTokenException.class
+    })
+    public ResponseEntity<ErrorResponse> handle401Errors(RuntimeException e) {
         ErrorResponse errorBody = ErrorResponse.of(HttpStatus.UNAUTHORIZED.value(), e.getMessage());
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorBody);
     }
+
 
 }
