6e7e673418c50d19c8ce2eda7776502fbc958d16
Lee Si Woong <silverttthin@naver.com>
2025-09-22 15:21:57 +0900

    feat: test api에 다른팀원 참고용으로 인가 기능 붙여둠

--- src/main/java/com/igemoney/igemoney_BE/fortest/testController.java
+++ src/main/java/com/igemoney/igemoney_BE/fortest/testController.java
@@ -1,16 +1,20 @@
 package com.igemoney.igemoney_BE.fortest;
 
 
+import com.igemoney.igemoney_BE.common.annotation.Authenticated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 
-// cd가 잘 작동되는지 확인하기 위한 임시 컨트롤러입니다. 추후 삭제예정입니다
+// fixme: cd가 잘 작동되는지 확인하기 위한 임시 컨트롤러입니다. 인가 기능 참고용으로 쓰고 8주차 즈음에 삭제예정입니다
 @RestController
 public class testController {
+
+	@Authenticated
 	@GetMapping("/test")
-	public String test() {
-		return "test";
+	public String test(@RequestAttribute("userId") Long userId) {
+		return "test " + userId;
 	}
 
 	@GetMapping
