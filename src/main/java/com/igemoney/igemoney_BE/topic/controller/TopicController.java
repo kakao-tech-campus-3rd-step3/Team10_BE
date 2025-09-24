e4794afad22278229f9045cfcdd0b8959eeabf07
Kwon <kwon@Kwonui-MacBookPro.local>
2025-09-24 18:49:30 +0900

    토픽에 해당하는 퀴즈목록 API 구현

--- src/main/java/com/igemoney/igemoney_BE/topic/controller/TopicController.java
+++ src/main/java/com/igemoney/igemoney_BE/topic/controller/TopicController.java
@@ -3,6 +3,7 @@ package com.igemoney.igemoney_BE.topic.controller;
 import com.igemoney.igemoney_BE.common.annotation.Authenticated;
 import com.igemoney.igemoney_BE.common.utils.JwtUtil;
 import com.igemoney.igemoney_BE.topic.dto.TopicCreateRequest;
+import com.igemoney.igemoney_BE.topic.dto.TopicQuizList;
 import com.igemoney.igemoney_BE.topic.dto.TopicResponse;
 import com.igemoney.igemoney_BE.topic.dto.UserTopicList;
 import com.igemoney.igemoney_BE.topic.service.TopicService;
@@ -37,5 +38,11 @@ public class TopicController {
     @GetMapping
     public UserTopicList getUserTopics(@RequestAttribute("userId") Long userId){
         return topicService.getUserTopics(userId);
+    }
+
+    @Authenticated
+    @GetMapping("/{topicId}")
+    public TopicQuizList getTopicQuizzes(@RequestAttribute("userId") Long userId, @PathVariable Long topicId){
+        return topicService.getTopicQuizzes(userId,topicId);
     }
 }
---
+++ src/main/java/com/igemoney/igemoney_BE/topic/dto/TopicQuizResponse.java
@@ -0,0 +1,11 @@
+package com.igemoney.igemoney_BE.topic.dto;
+
+import com.igemoney.igemoney_BE.quiz.entity.enums.DifficultyLevel;
+
+public record TopicQuizResponse(
+        Long quizId,
+        int questionOrder,
+        String questionTitle,
+        DifficultyLevel difficultyLevel,
+        boolean isSolved
+) {}
