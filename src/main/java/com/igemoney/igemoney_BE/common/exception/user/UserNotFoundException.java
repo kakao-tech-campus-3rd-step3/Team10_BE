2504c29a8a1a6238e3b8b550f199ee1121b5a52c
Lee Si Woong <silverttthin@naver.com>
2025-10-08 00:44:33 +0900

    feat: 유저 못찾는 예외 클래스에 디폴트 메시지 추가

--- src/main/java/com/igemoney/igemoney_BE/common/exception/user/UserNotFoundException.java
+++ src/main/java/com/igemoney/igemoney_BE/common/exception/user/UserNotFoundException.java
@@ -2,6 +2,12 @@ package com.igemoney.igemoney_BE.common.exception.user;
 
 public class UserNotFoundException extends RuntimeException {
 
+    private static final String DEFAULT_MESSAGE = "존재하지 않는 유저입니다.";
+
+    public UserNotFoundException() {
+        super(DEFAULT_MESSAGE);
+    }
+
     public UserNotFoundException(String message) {
         super(message);
     }
