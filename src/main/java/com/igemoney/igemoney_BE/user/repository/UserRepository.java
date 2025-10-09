1746afc300b63890189f17d1a48b1c83aa1e5169
hopencode <newalfo1006@gmail.com>
2025-10-09 23:55:35 +0900

    feat: user 레포지토리 상위 랭커 3명 조회 메서드 구현

--- src/main/java/com/igemoney/igemoney_BE/user/repository/UserRepository.java
+++ src/main/java/com/igemoney/igemoney_BE/user/repository/UserRepository.java
@@ -1,10 +1,17 @@
 package com.igemoney.igemoney_BE.user.repository;
 
 import com.igemoney.igemoney_BE.user.entity.User;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByKakaoOauthId(Long kakaoOauthId);
+
+    // 랭킹 서비스 관련
+    List<User> findTop3ByOrderByRatingPointDesc();
+
+    List<User> findTop3ByOrderByConsecutiveAttendanceDesc();
 }
