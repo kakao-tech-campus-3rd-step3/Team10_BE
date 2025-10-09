5ffc34bb46ddcf7462f58b02b7b22d1bd1ec058d
hopencode <newalfo1006@gmail.com>
2025-10-10 00:03:55 +0900

    refactor: 출석 예외 핸들러 RestControllerAdvice로 임시 수정     - body 포멧 구성은 글로벌 예외 핸들러 참조 또는 거기에 합칠 예정

--- src/main/java/com/igemoney/igemoney_BE/attendance/handler/AttendanceExceptionHandler.java
+++ src/main/java/com/igemoney/igemoney_BE/attendance/handler/AttendanceExceptionHandler.java
@@ -3,10 +3,10 @@ package com.igemoney.igemoney_BE.attendance.handler;
 import com.igemoney.igemoney_BE.attendance.exception.AttendanceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice(basePackages = "com.igemoney.igemoney_BE.attendance")
+@RestControllerAdvice(basePackages = "com.igemoney.igemoney_BE.attendance")
 public class AttendanceExceptionHandler {
     @ExceptionHandler(AttendanceNotFoundException.class)
     public ResponseEntity<String> handleAttendanceNotFound(AttendanceNotFoundException ex) {
