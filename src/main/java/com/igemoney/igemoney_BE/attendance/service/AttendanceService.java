6139c27d7600b582abb69cf6485492b1b1b4995d
hopencode <newalfo1006@gmail.com>
2025-10-11 09:54:25 +0900

    refactor: 멘토 피드백 반영 대량의 유저 대비 페이징 + 비동기 처리 적용

--- src/main/java/com/igemoney/igemoney_BE/attendance/service/AttendanceService.java
+++ src/main/java/com/igemoney/igemoney_BE/attendance/service/AttendanceService.java
@@ -3,21 +3,25 @@ package com.igemoney.igemoney_BE.attendance.service;
 import com.igemoney.igemoney_BE.user.entity.User;
 import com.igemoney.igemoney_BE.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class AttendanceService {
 
     private static final int TODAY_TARGET_COUNT = 5;
 
     private final UserRepository userRepository;
+    private final AsyncAttendanceService asyncAttendanceService;
 
+    @Transactional
     public void incrementTodaySolvedCount(Long kakaoOauthId) {
         User user = userRepository.findByKakaoOauthId(kakaoOauthId)
                 .orElseThrow(() -> new NoSuchElementException("User not found"));
@@ -31,13 +35,14 @@ public class AttendanceService {
     }
 
     public void resetAttendanceForAllUsers() {
-        List<User> users = userRepository.findAll();
-        for (User user : users) {
-            if (user.getTodayCount() < TODAY_TARGET_COUNT) {
-                user.resetConsecutiveAttendance();
-            }
-            user.resetTodaySolvedCount();
+        int pageSize = 10000;
+        long total = userRepository.count();
+        int totalPages = (int) Math.ceil((double) total / pageSize);
+
+        for (int i = 0; i < totalPages; i++) {
+            Pageable pageable = PageRequest.of(i, pageSize, Sort.by("userId").ascending());
+            Page<User> page = userRepository.findAll(pageable);
+            asyncAttendanceService.resetAndSaveUsers(page.getContent());
         }
-        userRepository.saveAll(users);
     }
 }
--- src/main/java/com/igemoney/igemoney_BE/user/repository/UserRepository.java
+++ src/main/java/com/igemoney/igemoney_BE/user/repository/UserRepository.java
@@ -4,11 +4,16 @@ import com.igemoney.igemoney_BE.user.entity.User;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByKakaoOauthId(Long kakaoOauthId);
+
+    Page<User> findAll(Pageable pageable);
 
     // 랭킹 서비스 관련
     List<User> findTop3ByOrderByRatingPointDesc();
