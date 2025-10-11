760a08a81ddadd81c8e789b6421ca568f5de8572
hopencode <newalfo1006@gmail.com>
2025-10-11 16:45:13 +0900

    feat: 랭킹 서비스 현재 유저 기준 위아래 2인 조회 레포지토리 메서드 구현

--- src/main/java/com/igemoney/igemoney_BE/user/repository/UserRepository.java
+++ src/main/java/com/igemoney/igemoney_BE/user/repository/UserRepository.java
@@ -18,7 +18,17 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Page<User> findAll(Pageable pageable);
 
     // 랭킹 서비스 관련
+    // RatingPoint 상위 3명
     List<User> findTop3ByOrderByRatingPointDesc();
 
+    // 현재 유저 기준 바로 위 2명
+    List<User> findTop2ByRatingPointGreaterThanOrderByRatingPointAsc(int ratingPoint);
+
+    // 현재 유저 기준 바로 아래 2명
+    List<User> findTop2ByRatingPointLessThanOrderByRatingPointDesc(int ratingPoint);
+
+    // 연속 출석 기준 랭킹
     List<User> findTop3ByOrderByConsecutiveAttendanceDesc();
+    List<User> findTop2ByRatingPointGreaterThanOrderByConsecutiveAttendanceAsc(int consecutiveAttendance);
+    List<User> findTop2ByRatingPointLessThanOrderByConsecutiveAttendanceDesc(int consecutiveAttendance);
 }
