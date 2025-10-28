c745b884b910b09f7d4f3380c3d5cd3f0e0f0e0e
hopencode <newalfo1006@gmail.com>
2025-10-29 01:51:30 +0900

    feat: 투자성향 진단 응답에 isTested 속성 추가

--- src/main/java/com/igemoney/igemoney_BE/user/dto/GetMyInvestmentPropensityResponseDto.java
+++ src/main/java/com/igemoney/igemoney_BE/user/dto/GetMyInvestmentPropensityResponseDto.java
@@ -4,5 +4,14 @@ import com.igemoney.igemoney_BE.propensity.type.InvestmentPropensity;
 
 public record GetMyInvestmentPropensityResponseDto(
         InvestmentPropensity propensity,
-        String propensityKoreanName
-) {}
+        String propensityKoreanName,
+        boolean isTested
+) {
+    public GetMyInvestmentPropensityResponseDto(InvestmentPropensity propensity) {
+        this(
+                propensity,
+                propensity.getLabel(),
+                propensity.isDiagnosed()
+        );
+    }
+}
